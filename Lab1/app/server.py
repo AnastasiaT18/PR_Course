e48f59b22b90d387f4b90571364092b3fe6b9d33
Anastasia <anastasia.tiganescu@isa.utm.md>
2025-10-08 22:10:42 +0200

    Implemented client, made subfolder of content for directory listing.

--- Lab1/app/server.py
+++ Lab1/app/server.py
@@ -23,8 +23,13 @@ def start_server():
         data = connectionSocket.recv(1024).decode()
 
         request = data.split("\r\n")[0]
+        print("this is the request", request, flush=True)
         requested_file = request.split(" ")[1]
+        print("this is req file", requested_file, flush=True)
         requested_file_path = os.path.join(content_dir, requested_file[1:])
+        print("this is req file path", requested_file_path, flush=True)
+        print("Absolute path:", os.path.abspath(requested_file_path),   flush=True)
+        print("isdir?", os.path.isdir(requested_file_path), flush=True)
 
         ext = os.path.splitext(requested_file_path)[1]
         
@@ -62,7 +67,39 @@ def start_server():
                     connectionSocket.sendall(body)  # body is bytes
                 else:
                     connectionSocket.sendall(body.encode())  # body is str
-               
+
+        elif os.path.isdir(requested_file_path):
+            print("it's a directory,", flush=True)
+            contents = os.listdir(requested_file_path)
+            print(contents)
+            items_html = ""
+            for item in contents:
+                if requested_file == "/":
+                    link_path = f"/{item}"
+                else:
+                    link_path = f"{requested_file}/{item}"
+                items_html += f'<li><a href="{link_path}">{item}</a></li>'
+
+            response_body = f"""
+                <html>
+                <body>
+                    <h1>Directory listing for {requested_file}</h1>
+                    <ul>
+                    {items_html}
+                    </ul>
+                </body>
+                </html>
+                """
+            
+            response_headers = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n"
+                f"Content-Length: {len(response_body)}\r\n"
+                "\r\n"
+            )
+            response = response_headers + response_body
+            connectionSocket.sendall(response.encode())
+
         else:
             response = (
                 "HTTP/1.1 404 Not Found\r\n"
